2e41354bcdbf5dac02c26fb73286fb32d32ff21f
Tal Livneh <talivneh8@gmail.com>
2021-11-28 17:37:52 +0200

    arrow-fix

--- src/components/ArrowUp.js
+++ src/components/ArrowUp.js
@@ -4,7 +4,7 @@ export default function ArrowUp() {
   return (
     <div className="flex justify-end items-center w-screen z-10 fixed">
       <a href="#Top">
-        <i className="fas fa-arrow-up text-xl text-twhite opacity-60 border-4 rounded-full px-2 py-1 mt-3 mr-3"></i>
+        <i className="fas fa-arrow-up text-xl text-twhite opacity-60 border-2 rounded-full px-1 mt-3 sm:mr-5 xl:mr-8 shadow-md"></i>
       </a>
     </div>
   );
